1adbfd60f0fec5efc3048dac0f0dd7675d12a793
Renzo Westerbeek <renzowesterbeek@gmail.com>
2014-08-27 21:59:10 +0200

    Add Animations

--- js/animations.js
+++ js/animations.js
@@ -1,11 +1,35 @@
 $(document).ready(function(){
+	var isOut = false;
 	
-	$( "#submit" ).click(function() {
+	function animateAnsIn() {
 		$("#ansContainer").animate({
 			marginTop: 0
-		}, 2000, function() {
-			// Animation complete.
-		});	
+		}, 500, function() {
+			isOut = true;
+		});
+	}
+	
+	function animateAnsOut() {
+		$("#ansContainer").animate({
+			marginTop: -75
+		}, 250, function() {
+			isOut = false;
+		});
+	}
+	
+	
+	
+	$( "#submit" ).click(function() {
+		if (isOut) {
+			animateAnsOut();
+			animateAnsIn();
+		} else {
+			animateAnsIn();
+		}
+	});
+	
+	$("#inputField").focus(function(){
+		animateAnsOut();
 	});
 
 });
